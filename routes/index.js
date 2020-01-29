cd4d047e3ff290a8d78c3e7c5ee67b9cf55c4382
Erin Johnson <erinjohnson47@gmail.com>
2020-01-28 16:39:31 -0800

    create simple search results view

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,38 @@
-var express = require('express');
-var router = express.Router();
-var us_states = require('../us_state.js');
+const express = require('express');
+const router = express.Router();
+const usStates = require('../us_state.js');
+// const apiData = require('../search.js')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Find My Election', states: us_states });
+router.get('/', (req, res, next) => {
+  try {
+    res.render('index', { 
+        title: 'Find My Upcoming Elections', 
+        states: usStates 
+      });
+  } catch (err) {
+    res.send(err);
+  }
 });
+
+router.post('/', (req, res, next) => {
+  try {
+    const data = req.body;
+    const dataObject = {
+      street: data.street,
+      street2: data.street2,
+      city: data.city,
+      state: data.state,
+      zip: data.zip
+    }
+    console.log(dataObject, 'ths is req.body')    
+    res.render('search', {
+      title: 'Local Election Search Results',
+      dataObject
+    });
+  } catch (error) {
+    console.error(error)
+  }
+})
 
 module.exports = router;
